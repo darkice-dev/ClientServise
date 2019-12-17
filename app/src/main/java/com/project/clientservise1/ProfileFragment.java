feb41c315580e747b5737ae43956680e1e50f0d3
Sasha Filippova <sa77728@gmail.com>
2019-12-17 22:28:30 +1000

    Implemented login button

--- app/src/main/java/com/project/clientservise1/ProfileFragment.java
+++ app/src/main/java/com/project/clientservise1/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.project.clientservise1;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -20,12 +22,28 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private Button btn_enter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
 
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+             // ваша кнопка
+        btn_enter = view.findViewById(R.id.btn_enter);
+            //создаем листенер
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //обработка нажатия
+                Intent intent = new Intent(getActivity(), EnterActivity.class);
+                startActivity(intent);
+            }
+        };
+            //привязываем листенер к кнопке
+        btn_enter.setOnClickListener(oclBtnOk);
+
+        return view;
+    }
 }
