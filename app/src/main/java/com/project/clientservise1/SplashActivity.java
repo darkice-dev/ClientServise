db69895af6b0dcc2d67a302c364bd8f508c55976
Sasha Filippova <sa77728@gmail.com>
2020-01-02 19:01:02 +1000

    Added Splash activity and changed list button in HomeFragment

---
+++ app/src/main/java/com/project/clientservise1/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.project.clientservise1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.os.Handler;
+//import java.util.logging.LogRecord;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {                                              //Позволяет выводить окно загрузки какое то время
+            @Override                                                                           //
+            public void run() {                                                                 //
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);   //
+                startActivity(i);                                                               //
+                finish();                                                                       //
+            }                                                                                   //
+        }, 3*500);                                                                    //
+
+    }
+}
